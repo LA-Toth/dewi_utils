5ba209964da9f181cc4d911eec1b010ea394fac7
Laszlo Attila Toth <panther@elte.hu>
2012-06-17 17:23:10 +0200

    Commands: using command registry
    The command classes are not loaded directly into Commands module,
    instead they are registered into the main command registry
    
    Signed-off-by: Laszlo Attila Toth <panther@elte.hu>

--- lib/DWA/Commands/__init__.py
+++ lib/DWA/Commands/__init__.py
@@ -1,1 +1,7 @@
-from .Checkout import CheckoutCommand
+from DWA.Core.State import main_command_registry
+
+def __initialize():
+    from .Checkout import CheckoutCommand
+    main_command_registry.register_command_class(CheckoutCommand.get_name(), CheckoutCommand)
+
+__initialize()
