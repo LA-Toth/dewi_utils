9c10f35fce2315abd63e6157b5ea7030780c063b
Laszlo Attila Toth <panther@elte.hu>
2012-06-17 17:22:19 +0200

    State: added set_root_dir() and a global command registry
    The commands are stored in main_command_registry.
    
    Signed-off-by: Laszlo Attila Toth <panther@elte.hu>

--- lib/DWA/Core/State.py
+++ lib/DWA/Core/State.py
@@ -1,6 +1,17 @@
+
+__all__ = ['get_dir', 'set_dir', 'main_command_registry']
+from DWA.Core.CommandRegistry import CommandRegistry
 root_dir = '/tmp'
+
+main_command_registry = CommandRegistry()
+
 
 def get_dir(name):
     if len(name) and name[0] == '/':
         name = name[1:]
     return '{}/{}'.format(root_dir, name)
+
+
+def set_root_dir(new_root_dir):
+    global root_dir
+    root_dir = new_root_dir
