5f317fee253682d97282efbdd2730192a8113e70
Laszlo Attila Toth <panther@elte.hu>
2012-06-23 17:43:46 +0200

    State: added main_config; implemented register_command_class

--- lib/DWA/Core/State.py
+++ lib/DWA/Core/State.py
@@ -1,10 +1,11 @@
+from DWA.Config.YamlConfig import YamlConfig
 
-__all__ = ['get_dir', 'set_dir', 'main_command_registry']
+__all__ = ['get_dir', 'set_dir', 'main_command_registry', 'main_config', 'register_command_class']
 from DWA.Core.CommandRegistry import CommandRegistry
 root_dir = '/tmp'
 
 main_command_registry = CommandRegistry()
-
+main_config = YamlConfig()
 
 def get_dir(name):
     if len(name) and name[0] == '/':
@@ -15,3 +16,11 @@ def get_dir(name):
 def set_root_dir(new_root_dir):
     global root_dir
     root_dir = new_root_dir
+
+
+def register_command_class(name, cls):
+    global main_command_registry
+    main_command_registry.register_command_class(name, cls)
+
+
+main_config.open(get_dir('/etc/config.yml'))
