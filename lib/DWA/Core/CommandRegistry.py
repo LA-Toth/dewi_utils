44e030e9060f4129268d285d90c65914c431faf3
Laszlo Attila Toth <panther@elte.hu>
2012-06-30 11:21:57 +0200

    CommandRegistry: more verbose exceptions, including cmd name

--- lib/DWA/Core/CommandRegistry.py
+++ lib/DWA/Core/CommandRegistry.py
@@ -13,12 +13,12 @@ class CommandRegistry(object):
 
     def register_command_class(self, name, cls):
         if name in self.__registry:
-            raise CommandRegistryException("Command class name is already used")
+            raise CommandRegistryException("Command class name is already used; name='{}'".format(name))
         self.__registry[name] = cls
 
     def get_command_class(self, name):
         if name not in self.__registry:
-            raise CommandRegistryException("Specified command class name is not found")
+            raise CommandRegistryException("Specified command class name is not found; name='{}'".format(name))
         return self.__registry[name]
 
     def get_command_count(self):
