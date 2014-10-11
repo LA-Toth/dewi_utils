df5ac8c766fb1790ae25f0199daeaa968b71512a
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2014-10-11 17:23:00 +0200

    wrap unittest.TestCase, use lowercased assertion methods

--- dewi/tests/test_main.py
+++ dewi/tests/test_main.py
@@ -1,10 +1,10 @@
-import unittest
 import unittest.mock
 from dewi.main import main
+import dewi.tests
 
 
 @unittest.mock.patch('dewi.main.myprint')
-class MainTest(unittest.TestCase):
+class MainTest(dewi.tests.TestCase):
     def test_main(self, print_mock: unittest.mock.Mock):
         main()
         print_mock.assert_called_once_with('DEWI is started')
