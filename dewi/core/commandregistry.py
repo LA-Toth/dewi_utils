517bf3e0713969e7abc8356a7e6c3f3e26cecd97
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2015-01-30 16:24:20 +0100

    core/commandregistry: add type annotations

--- dewi/core/commandregistry.py
+++ dewi/core/commandregistry.py
@@ -1,4 +1,6 @@
+import collections
 import sys
+from dewi.core.command import Command
 
 
 class CommandRegistryException(Exception):
@@ -27,12 +29,12 @@ class ClassDescriptorWithModuleAndClassName(ClassDescriptor):
     """
     Both the module name and the command class name are specified
     """
-    def __init__(self, module_name, class_name):
+    def __init__(self, module_name: str, class_name: str):
         super(ClassDescriptorWithModuleAndClassName, self).__init__()
         self._module_name = module_name
         self._class_name = class_name
 
-    def get_class(self):
+    def get_class(self) -> Command:
         module = self._get_module_object()
         try:
             return getattr(module, self._class_name)
@@ -43,17 +45,17 @@ class ClassDescriptorWithModuleAndClassName(ClassDescriptor):
         __import__(self._module_name)
         return sys.modules[self._module_name]
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.get_class().name
 
 
 class ClassDescriptorWithModuleName(ClassDescriptorWithModuleAndClassName):
     """The module and the class has the same name (the module's last part at least)"""
 
-    def __init__(self, module_name):
+    def __init__(self, module_name: str):
         super(ClassDescriptorWithModuleName, self).__init__(module_name, module_name.split('.')[-1])
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.get_class().name
 
 
@@ -62,10 +64,10 @@ class ClassDescriptorWithModuleNameAndCmdclassMember(ClassDescriptorWithModuleAn
     The command is in a module that have specific layout, its __init__.py has commandlcass member. This member
     stores the command class.
     """
-    def __init__(self, module_name):
+    def __init__(self, module_name: str):
         super(ClassDescriptorWithModuleNameAndCmdclassMember, self).__init__(module_name, 'commandclass')
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self._get_module_object().name
 
 
@@ -73,14 +75,14 @@ class ClassDescriptorWithConcreteClass(ClassDescriptor):
     """
     This descriptor stores the class itself, mainly used for testing.
     """
-    def __init__(self, class_object):
+    def __init__(self, class_object: Command):
         super(ClassDescriptorWithConcreteClass, self).__init__()
         self.class_object = class_object
 
-    def get_class(self):
+    def get_class(self) -> Command:
         return self.class_object
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.class_object.name
 
 
@@ -92,7 +94,7 @@ class CommandRegistry(object):
     def __init__(self):
         self.__registry = dict()
 
-    def __validate_name_and_class_descriptor(self, name, class_descriptor):
+    def __validate_name_and_class_descriptor(self, name: str, class_descriptor: ClassDescriptor):
         if name in self.__registry:
             raise CommandRegistryException("Command class name is already used; name='{0}'".format(name))
         if not isinstance(class_descriptor, ClassDescriptor):
@@ -100,17 +102,17 @@ class CommandRegistry(object):
                 "Command class descriptor's type differs from ClassDescriptor; "
                 "type='{0}'".format(type(class_descriptor).__name__))
 
-    def register_command_class(self, name, class_descriptor):
+    def register_command_class(self, name: str, class_descriptor: ClassDescriptor):
         self.__validate_name_and_class_descriptor(name, class_descriptor)
         self.__registry[name] = class_descriptor
 
-    def get_command_class_descriptor(self, name):
+    def get_command_class_descriptor(self, name: str) -> ClassDescriptor:
         if name not in self.__registry:
             raise CommandRegistryException("Specified command class name is not found; name='{0}'".format(name))
         return self.__registry[name]
 
-    def get_command_count(self):
+    def get_command_count(self) -> int:
         return len(self.__registry)
 
-    def get_command_names(self):
+    def get_command_names(self) -> collections.Sequence:
         return list(self.__registry.keys())
