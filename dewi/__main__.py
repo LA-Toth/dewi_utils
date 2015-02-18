11b83105b1b0bf963fb904924a6f9f8ad4a17421
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2015-02-18 17:35:50 +0100

    Added command-line tool and wire in basic argument parsing
    From now on DEWI is usable from command line, in the following way:
     dewi-local -p dewi.commands.sample.SamplePlugin sample
    
    dewi-local is simply a wrapper around the dewi module, and it is not needed
    when DEWI is installed from pip or something like that.
    
    The -p / --plugin option is for registering plugins. By default the new
    DewiPlugin is registered, but this can be replaced via command-line args.
    The first argument is the command to be run.

---
+++ dewi/__main__.py
@@ -0,0 +1,23 @@
+from argparse import ArgumentParser
+import collections
+import sys
+from dewi.core.application import MainApplication
+from dewi.loader.loader import PluginLoader
+
+
+def main():
+    parser = ArgumentParser(add_help=False)
+    parser.add_argument(
+        '-p', '--plugin', help='Load this plugin. This option can be specified more than once.',
+        default=[], action='append')
+
+    opts, args = parser.parse_known_args(sys.argv[1:])
+    _main(opts.plugin or {'dewi.core.application.DewiPlugin'}, args)
+
+
+def _main(plugins: collections.Iterable, args: collections.UserList):
+    app = MainApplication()
+    sys.exit(app.run(plugins, args))
+
+if __name__ == '__main__':
+    main()
---
+++ dewi/core/application.py
@@ -0,0 +1,34 @@
+import collections
+from dewi.core.commandregistry import CommandRegistryException
+
+from dewi.core.context import Context
+from dewi.loader.loader import PluginLoader, PluginLoaderError
+from dewi.loader.plugin import Plugin
+
+
+class DewiPlugin(Plugin):
+    def get_description(self) -> str:
+        return "DEWI application plugin"
+
+    def get_dependencies(self) -> collections.Iterable:
+        return {'dewi.core.CorePlugin'}
+
+    def load(self, c: Context):
+        pass
+
+
+class MainApplication:
+    def run(self, plugins: collections.Iterable, args: collections.UserList):
+        if len(args) < 1:
+            print("Required parameter, command is missing")
+            return 1
+
+        loader = PluginLoader()
+        try:
+            context = loader.load(set(plugins))
+            command_name = args[0]
+            desc = context['commandregistry'].get_command_class_descriptor(command_name)
+            return desc.get_class()().perform(args[1:])
+        except (CommandRegistryException, PluginLoaderError) as exc:
+            print(str(exc))
+            return 1
