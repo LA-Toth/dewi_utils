711caf8339180ee4fb0db4b9666ad2c7b989a41c
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2016-11-29 17:35:06 +0100

    dewi/utils/render.py: add Jinja2 TemplateRenderer
    Jinja2 template renderer is added, with FileSystemLoader.
    The search path is sys.path, but it can be overriden by the base_path
    parameter, which can be a string or list of strings.

---
+++ dewi/utils/render.py
@@ -0,0 +1,17 @@
+import collections
+import sys
+import typing
+
+import jinja2
+
+
+class TemplateRenderer:
+    def __init__(self, base_path: typing.Union[str, typing.List[str]]):
+        loader = jinja2.FileSystemLoader(base_path or sys.path)
+        environment = jinja2.Environment(loader=loader)
+        self._environment = environment
+
+    def render(self, template_file_path: str, template_variables: collections.Mapping) -> str:
+        template = self._environment.get_template(template_file_path)
+
+        return template.render(**template_variables)
