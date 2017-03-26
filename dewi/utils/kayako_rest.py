e899a84896741e0085f34cd6c2db9213690ef5b1
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2017-03-26 14:20:09 +0200

    dewi/utils: add kayako_rest to communicate with a Kayako ticketing system
    The classes for using Kayako REST API - which is not RESTful,
    unfortunately - are the following:
    
    * Host: describes the access of the API, such as secret, host name
      The code assumes that SSL is in use, therefore added an option
      to skip the SSL verification - e.g. for a test host
    * Request: a one-shot HTTP request-response, to query the API,
      or POST/PUT data, and fetch the result of these API calls
    * get_response_as_dict basic API is XML-based, this function can
      fetch (HTTP GET) data and convert the XML result to a dict using
      dewi.utils.xml.

---
+++ dewi/utils/kayako_rest.py
@@ -0,0 +1,90 @@
+# Copyright (C) 2017 Laszlo Attila Toth
+# Distributed under the terms of GNU General Public License v3
+
+
+import base64
+import hashlib
+import hmac
+import random
+import ssl
+import typing
+import urllib.parse
+import urllib.request
+
+from dewi.utils.xml import create_dict_from_xml_string
+
+
+class AlreadyProcessed(RuntimeError):
+    pass
+
+
+class Host:
+    def __init__(self, api_url: str, api_key: str, secret: str, unsafe_ssl: bool = False):
+        self.api_url = api_url
+        self.api_key = api_key
+        self.secret = secret
+        self.unsafe_ssl = unsafe_ssl
+
+
+class Request:
+    def __init__(self, host: Host, endpoint: str, params: typing.Optional[dict] = None):
+        self.api_host = host
+        self.salt = str(random.getrandbits(32))
+        self.signature = self._generate_signature()
+        self.endpoint = endpoint
+        self.params = dict(params) if params else dict()
+        self.url = self.api_host.api_url + '?e=' + self.endpoint
+        self.params['apikey'] = self.api_host.api_key
+        self.params['salt'] = self.salt
+        self.params['signature'] = self.signature
+        self._processed_once = False
+
+    def _generate_signature(self):
+        digest = hmac.new(
+            self.api_host.secret.encode('UTF-8'),
+            msg=self.salt.encode('UTF-8'), digestmod=hashlib.sha256).digest()
+        return base64.b64encode(digest)
+
+    def _generate_url(self):
+        self.url += '&' + urllib.parse.urlencode(self.params)
+
+    def _urlopen(self, *args, **kwargs):
+        if self.api_host.unsafe_ssl:
+            ctx = ssl.create_default_context()
+            ctx.check_hostname = False
+            ctx.verify_mode = ssl.CERT_NONE
+
+            kwargs.update(dict(context=ctx))
+
+        return urllib.request.urlopen(*args, **kwargs)
+
+    def fetch(self):
+        self._check_if_processed()
+        self._generate_url()
+        with self._urlopen(self.url) as response:
+            return response.read()
+
+    def put(self):
+        self._check_if_processed()
+        request = urllib.request.Request(self.url, method='PUT',
+                                         data=urllib.parse.urlencode(self.params).encode('UTF-8'))
+        with self._urlopen(request) as response:
+            return response.read()
+
+    def post(self):
+        self._check_if_processed()
+        request = urllib.request.Request(self.url, method='POST',
+                                         data=urllib.parse.urlencode(self.params).encode('UTF-8'))
+        with self._urlopen(request) as response:
+            return response.read()
+
+    def _check_if_processed(self):
+        if self._processed_once:
+            raise AlreadyProcessed()
+
+        self._processed_once = True
+
+
+def get_response_as_dict(h: Host, endpoint: str, params: typing.Optional[dict] = None):
+    r = Request(h, endpoint, params)
+    return create_dict_from_xml_string(r.fetch().decode('UTF-8'))
