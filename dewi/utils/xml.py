f1d24fad377a3d36fd7e34f20798dbc93777941d
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2017-03-26 14:20:09 +0200

    dewi/utils: add xml module to convert an XML document to a dict
    While it seems that it is a good idea to convert an XML document to a dict,
    it has some limitations, especially when an element is repeated
    unexpectedly, therefore in the result some of the entries might be lists,
    if an element is repeated, otherwise the content of the element is used
    there. In all other cases the behavior is straightforward.

---
+++ dewi/utils/xml.py
@@ -0,0 +1,56 @@
+# Copyright (C) 2017 Laszlo Attila Toth
+# Distributed under the terms of GNU General Public License v3
+from xml.etree import ElementTree
+
+import typing
+
+
+def create_dict_from_xml_string(xml: str):
+    root = ElementTree.fromstring(xml)
+    return create_dict_from_xml_element(root)
+
+
+def create_dict_from_xml_element(elem: ElementTree):
+    return _add_as_dict(elem)
+
+
+def _add_as_list(elem: ElementTree):
+    l = list()
+    for child_item in elem:
+        if child_item:
+            if len(child_item) == 1:
+                l.append(_add_as_dict(child_item))
+            if len(child_item) > 1:
+                if child_item[0].tag != child_item[1].tag:
+                    l.append(_add_as_dict(child_item))
+                else:
+                    l.append(_add_as_list(child_item))
+        elif child_item.text:
+            l.append(child_item.text)
+    return l
+
+
+def _add_as_dict(elem: ElementTree) -> typing.Union[dict, str]:
+    value = dict()
+    if len(list(elem)):
+        for child_item in list(elem):
+            if child_item.tag in value:
+                if not isinstance(value[child_item.tag], list):
+                    value[child_item.tag] = [value[child_item.tag]]
+                value[child_item.tag].append(_add_as_dict(child_item))
+            else:
+                value[child_item.tag] = _add_as_dict(child_item)
+        if elem.items():
+            value['_attrs'] = dict(elem.items())
+        if elem.text:
+            value['text'] = elem.text
+
+    elif elem.items():
+        value = dict(_attrs=dict(elem.items()))
+        if elem.text:
+            value['text'] = elem.text
+
+    elif elem.text:
+        value = elem.text
+
+    return value
