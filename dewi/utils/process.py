f1651738d57f9b59edca85d8710af635289cbf75
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2017-03-26 14:20:09 +0200

    dewi/utils: add process, find_binary/which to search for executable in PATH
    The PATH variable contains directories that can contain executable
    programs, or binaries. The find_binary() method searches a binary in
    these directories.
    
    On Windows, the binary name may be with or without extension, and if
    it is without it, the PATHEXT variable contains the list of extensions
    to be append when the binary lookup occurs.
    
    I saw a more basic implementation in Gentoo's portage, and I decided
    to use the same name, because it's more intuitive name than the shell's
    name for it, 'which', which is good for shell, but can be misleading
    in a Python software.
    
    Despite this, I also added a wrapper method to this lookup, called
    as the shell's name, 'which'.

---
+++ dewi/utils/process.py
@@ -0,0 +1,46 @@
+# Copyright (C) 2017 Laszlo Attila Toth
+# Distributed under the terms of GNU General Public License v3
+
+import os
+import sys
+import typing
+
+
+def find_binary(binary: str, *, exact: bool = False) -> typing.Optional[str]:
+    """
+    Find binary in PATH.
+
+    On UNIX systems 'binary' must match the basename of the found binary.
+    On Windows depending the 'exact' parameter:
+      * exact=True: the basename must match
+      * exact=False: the basename must match or appending the extensions in PATHEXT the extended name must match
+
+    An example:
+    find_binary('python') results either '/usr/bin/python' or 'C:\Program Files\Python\Python35\python.exe'
+    find_binary('python', exact=True) results '/usr/bin/python' on Unix or None on Windows
+
+    Args:
+        binary (str): basename - command - to be found. On Windows, it can be with or without extension
+        exact (bool): on Windows, if it is False, use PATHEXT variable, too
+
+    Returns:
+        str|None: The full path of the binary if found, otherwise None
+    """
+
+    paths = os.environ.get('PATH', '').split(os.path.pathsep)
+
+    path_exts = ['']
+    if sys.platform == 'win32' and not exact:
+        path_exts += os.environ.get('PATHEXT', '').split(os.path.pathsep)
+
+    for path in sorted(paths):
+        for path_ext in path_exts:
+            filename = os.path.join(path, binary + path_ext)
+            if os.access(filename, os.X_OK) and os.path.isfile(filename):
+                return filename
+
+    return None
+
+
+def which(binary: str, *, exact: bool = False) -> typing.Optional[str]:
+    return find_binary(binary, exact=exact)
