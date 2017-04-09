7daf42db37dbfbb07a080ed6db737aef41509dec
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2017-04-09 15:16:29 +0200

    xml: fix codestyle issue

--- dewi/utils/xml.py
+++ dewi/utils/xml.py
@@ -14,21 +14,21 @@ def create_dict_from_xml_element(elem: ElementTree):
 
 
 def _add_as_list(elem: ElementTree):
-    l = list()
+    result = list()
     for child_item in elem:
         if child_item:
             if len(child_item) == 1:
-                l.append(_add_as_dict(child_item))
+                result.append(_add_as_dict(child_item))
             if len(child_item) > 1:
                 if child_item[0].tag != child_item[1].tag:
-                    l.append(_add_as_dict(child_item))
+                    result.append(_add_as_dict(child_item))
                 else:
-                    l.append(_add_as_list(child_item))
+                    result.append(_add_as_list(child_item))
         elif child_item.text:
             text = child_item.text.strip()
             if text:
-                l.append(text)
-    return l
+                result.append(text)
+    return result
 
 
 def _add_as_dict(elem: ElementTree) -> typing.Union[dict, str]:
