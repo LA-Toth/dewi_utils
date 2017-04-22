e13da6d1401f1d3d020f99a6958f28ad74749630
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2017-04-22 15:01:31 +0200

    dewi.realtime_sync.watchers: became a package (original is in .watchers)
    Current module does too many things and there are too many types of
    watchers, it's better to split it. First step is to create an
    identical package.

---
+++ dewi/realtime_sync/watchers/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2017 Tóth, László Attila
+# Distributed under the terms of GNU General Public License v3
+# The license can be found in COPYING file or on http://www.gnu.org/licenses/
+
+
+from .watchers import *
