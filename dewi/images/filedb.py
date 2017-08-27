d91d187c14fc9759d4e901a57ebb544e9a5f36b0
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2017-08-27 21:58:01 +0200

    images: filedb: builtin types are uppercase + add support of new (target) path
    * DB creation cleanup: TEXT and other types are uppercase
    * add support of target path - file entry (rowid) map
    * add helper functions for this

--- dewi/images/filedb.py
+++ dewi/images/filedb.py
@@ -2,6 +2,7 @@
 # Distributed under the terms of GNU  General Public License v3
 # The license can be found in COPYING file or on http://www.gnu.org/licenses/
 
+import os.path
 import sqlite3
 import typing
 
@@ -21,12 +22,12 @@ class FileDatabase:
         c = self._conn.cursor()
 
         c.execute('''CREATE TABLE IF NOT EXISTS photo_file_info (
-                            orig_filename text,
-                            mod_date_as_dirname text,
-                            upper_basename text,
-                            filesize integer,
-                            mod_date integer,
-                            checksum text
+                            orig_filename TEXT,
+                            mod_date_as_dirname TEXT,
+                            upper_basename TEXT,
+                            filesize INTEGER,
+                            mod_date INTEGER,
+                            checksum TEXT
                      )''')
 
         c.execute('''CREATE INDEX IF NOT EXISTS photo_orig_file_name_idx ON photo_file_info (orig_filename)''')
@@ -34,6 +35,18 @@ class FileDatabase:
                                   ON photo_file_info
                                   (orig_filename, upper_basename, filesize, mod_date, checksum)
                            ''')
+
+        c.execute('''CREATE TABLE IF NOT EXISTS photo_file_info_new_path_map (
+                            file_info_id INTEGER,
+                            new_filename TEXT,
+                            FOREIGN KEY(file_info_id) REFERENCES photo_file_info(rowid)
+                      )''')
+
+        c.execute('''CREATE UNIQUE INDEX IF NOT EXISTS photo_file_info_new_path_map_uniq_rows
+                                      ON photo_file_info_new_path_map
+                                      (file_info_id, new_filename)
+                               ''')
+
         self._conn.commit()
 
     def close(self):
@@ -58,6 +71,14 @@ class FileDatabase:
                            ])
         self._changed = True
 
+    def insert_new_path(self, file_info_id: int, path: str):
+        self._conn.execute('INSERT INTO photo_file_info_new_path_map VALUES(?, ?)', [file_info_id, path])
+        self._changed = True
+
+    def has_new_path_info(self):
+        c = self._conn.cursor()
+        return c.execute('SELECT count(*) FROM photo_file_info_new_path_map').fetchone()[0] > 0
+
     def iterate(self):
         c = self._conn.cursor()
         for row in c.execute('SELECT *,rowid FROM photo_file_info ORDER BY orig_filename'):
@@ -68,6 +89,14 @@ class FileDatabase:
         for db_entry in c.execute('SELECT *,rowid FROM photo_file_info ORDER BY orig_filename'):
             yield FileEntry(db_entry[0], os.path.basename(db_entry[0]),
                             db_entry[2], db_entry[4], db_entry[3], db_entry[5], db_entry[6])
+
+    def iterate_target_path_entries(self) -> typing.Iterable[typing.Tuple[str, FileEntry]]:
+        c = self._conn.cursor()
+        c2 = self._conn.cursor()
+        for entry_id, path in c.execute('SELECT * FROM photo_file_info_new_path_map ORDER BY new_filename'):
+            db_entry = c2.execute('SELECT  *,rowid FROM photo_file_info WHERE rowid=?', (entry_id,)).fetchone()
+            yield path, FileEntry(db_entry[0], os.path.basename(db_entry[0]),
+                                  db_entry[2], db_entry[4], db_entry[3], db_entry[5], db_entry[6])
 
     def __contains__(self, item: str):
         c = self._conn.cursor()
