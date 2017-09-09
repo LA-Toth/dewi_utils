31cfe2c9dee410589293f3a0f4e4bdeafa9b795a
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2017-09-09 13:31:28 +0200

    dewi.commands.__init__: add ImageHandlerCommandsPlugin; remove unneeded deps
    ImageHandlerCommandsPlugin contains the comamnds for image sorting, namely:
    - collect-images
    - select-images
    - dedup-images
    - safe-delete-images
    
    The original CommandsPlugin depends on this new Plugin.
    The unnecessary dependency, CorePlugin is removed, as it is used
    by the command plugins, therefore it was redudnant.

--- dewi/commands/__init__.py
+++ dewi/commands/__init__.py
@@ -7,19 +7,31 @@ from dewi.core.context import Context
 from dewi.loader.plugin import Plugin
 
 
+class ImageHandlerCommandsPlugin(Plugin):
+    def get_description(self):
+        return "Commands to collect / sort / copy / delete images (photos)"
+
+    def get_dependencies(self) -> collections.Iterable:
+        return {
+            'dewi.commands.collect_images.ImageCollectorPlugin',
+            'dewi.commands.deduplicate_images.ImageDeduplicatorPlugin',
+            'dewi.commands.safe_delete_images.SafeEraserPlugin',
+            'dewi.commands.select_images.ImageSelectorPlugin',
+        }
+
+    def load(self, c: Context):
+        pass
+
+
 class CommandsPlugin(Plugin):
     def get_description(self) -> str:
         return "Commnands of DEWI"
 
     def get_dependencies(self) -> collections.Iterable:
         return {
-            'dewi.core.CorePlugin',
-            'dewi.commands.collect_images.ImageCollectorPlugin',
-            'dewi.commands.deduplicate_images.ImageDeduplicatorPlugin',
+            'dewi.commands.ImageHandlerCommandsPlugin',
             'dewi.commands.edit.edit.EditPlugin',
             'dewi.commands.filesync.FileSyncPlugin',
-            'dewi.commands.safe_delete_images.SafeEraserPlugin',
-            'dewi.commands.select_images.ImageSelectorPlugin',
             'dewi.commands.split_zorp_log.SplitZorpLogPlugin',
             'dewi.commands.ssh_ubuntu_windows.SshToUbuntuOnWindowsPlugin',
         }
