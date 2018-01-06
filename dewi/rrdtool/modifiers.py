6a7f1aca3d669b007717b39a3bc5877cffd5eb4e
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2018-01-06 14:28:38 +0100

    rrdtool/modifiers: add classes to rewrite some diskstats plugins
    diskstats_iops and throughput is affected:
    
    * RewriteDiskstatsLabels: rewrite labels to read / write, as
      both had the same labels (eg. sda for /dev/sda)
    * SeparateDiskstatsPluginsPerDevice: similar to the above, but
      makes separate plugins for every device, such as for sda and sdb.
      The original plugins are deleted.

--- dewi/rrdtool/modifiers.py
+++ dewi/rrdtool/modifiers.py
@@ -1,5 +1,7 @@
 # Copyright 2017-2018 Laszlo Attila Toth
 # Distributed under the terms of the GNU Lesser General Public License v3
+import re
+import typing
 
 from dewi.rrdtool.config import GraphConfig, Host, Plugin
 
@@ -24,7 +26,94 @@ class ConfigModifier:
 
 
 class IgnoreLoopbackDisks(ConfigModifier):
-    def modify_plugin(self, plugin: Plugin):
+    def _modify_plugin(self, plugin: Plugin):
         if plugin.category != 'disk':
             return
         plugin.field_order = [x for x in plugin.field_order if not x.startswith('loop')]
+
+
+class RewriteDiskstatsLabels(ConfigModifier):
+    ENDINGS = ['iops', 'throughput']
+    PLUGIN_NAMES = [f'diskstats_{x}' for x in ENDINGS]
+
+    def _modify_plugin(self, plugin: Plugin):
+        if plugin.category != 'disk' or plugin.name not in self.PLUGIN_NAMES:
+            return
+
+        plugin.options['graph_vlabel'] = 'IO/second' if plugin.name.endswith('iops') else 'Bytes/second'
+
+        for f in plugin.field_order:
+            field = plugin.fields[f]
+
+            if field.name.endswith('_rdio'):
+                field.options['label'] = 'Read IO ({})'.format(field.options['label'])
+            elif field.name.endswith('_wrio'):
+                field.options['label'] = 'Written IO ({})'.format(field.options['label'])
+            elif field.name.endswith('_rdbytes'):
+                field.options['label'] = 'Read bytes ({})'.format(field.options['label'])
+            elif field.name.endswith('_wrbytes'):
+                field.options['label'] = 'Written bytes ({})'.format(field.options['label'])
+
+
+class SeparateDiskstatsPluginsPerDevice(ConfigModifier):
+    ENDINGS = ['iops', 'throughput']
+    PLUGIN_NAMES = [f'diskstats_{x}' for x in ENDINGS]
+
+    def modify(self, config: GraphConfig):
+        self._modify_hosts(config)
+
+    def _modify_host(self, host: Host):
+        for p in host.plugins:
+            plugin = host.plugins[p]
+            if plugin.category != 'disk' or plugin.name not in self.PLUGIN_NAMES:
+                continue
+
+            new_plugins = self._rewrite_plugin(plugin)
+
+            del host.plugins[p]
+
+            for new_plugin in new_plugins:
+                host.plugins[new_plugin.name] = new_plugin
+
+    def _rewrite_plugin(self, plugin: Plugin) -> typing.List[Plugin]:
+        result = list()
+
+        graph_type = 'IO' if plugin.name.endswith('iops') else 'Bytes'
+
+        for f in plugin.field_order:
+            m = re.match(r'^(.+)_rd(io|bytes)$', f)
+            if not m:
+                continue
+
+            device = m.group(1)
+            read_name = f
+            write_name = re.sub(r'^(.*_)rd(io|bytes)$', r'\1wr\2', read_name)
+
+            new_plugin = Plugin()
+            result.append(new_plugin)
+
+            if plugin.name.endswith('_iops'):
+                new_plugin.title = f'Disk IOs for /dev/{device}'
+            else:
+                new_plugin.title = f'Disk throughput for /dev/{device}'
+            new_plugin.name = f'{plugin.name}_{device}'
+            new_plugin.category = plugin.category
+            new_plugin.period = plugin.period
+            new_plugin.field_order = 'read write'.split()
+            new_plugin.options = {
+                'graph_args': plugin.options['graph_args'],
+                'graph_vlabel': f'{graph_type}/second',
+            }
+
+            rfield = plugin.fields[read_name]
+            rfield.name = 'read'
+            rfield.options['label'] = 'Read ' + graph_type
+
+            wfield = plugin.fields[write_name]
+            wfield.name = 'write'
+            wfield.options['label'] = 'Written ' + graph_type
+
+            new_plugin.fields[rfield.name] = rfield
+            new_plugin.fields[wfield.name] = wfield
+
+        return result
