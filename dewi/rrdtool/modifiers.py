fe378ef39f07e9882d87f14db96752f9c5d993d5
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2018-01-06 14:26:04 +0100

    rrdtool/modifier: add methods to iterate hosts, plugins & make them protected
    ConfigModifier has only one public method, modify(), everything else
    is protected, with leading underscore character in their name.
    
    The default behaviour is extracted into _modify_plugins(), which calls
    _modify_plugin(). Another pair is added similarly, with names
    _modify_hosts() and _modify_host() respectively.

--- dewi/rrdtool/modifiers.py
+++ dewi/rrdtool/modifiers.py
@@ -1,15 +1,25 @@
-# Copyright 2017 Laszlo Attila Toth
+# Copyright 2017-2018 Laszlo Attila Toth
 # Distributed under the terms of the GNU Lesser General Public License v3
 
-from dewi.rrdtool.config import GraphConfig, Plugin
+from dewi.rrdtool.config import GraphConfig, Host, Plugin
 
 
 class ConfigModifier:
     def modify(self, config: GraphConfig):
-        for d, h, plugin_name in config.plugins:
-            self.modify_plugin(config.domains[d].hosts[h].plugins[plugin_name])
+        self._modify_plugins(config)
 
-    def modify_plugin(self, plugin: Plugin):
+    def _modify_plugins(self, config: GraphConfig):
+        for d, h, plugin_name in config.plugins:
+            self._modify_plugin(config.domains[d].hosts[h].plugins[plugin_name])
+
+    def _modify_hosts(self, config: GraphConfig):
+        for d, h in config.hosts:
+            self._modify_host(config.domains[d].hosts[h])
+
+    def _modify_plugin(self, plugin: Plugin):
+        pass
+
+    def _modify_host(self, host: Host):
         pass
 
 
