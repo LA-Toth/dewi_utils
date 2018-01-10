2617e7f685caf6a556cb0e9cc3f0f78d851e30cf
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2018-01-10 16:13:35 +0100

    Released version 1.1

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     * logparser: parse log files by modules based on the generic modules and emit messages
     """,
     license="LGPLv3",
-    version="1.0",
+    version="1.1",
     author="Laszlo Attila Toth",
     author_email="python-dewi@laszloattilatoth.me",
     maintainer="Laszlo Attila Toth",
