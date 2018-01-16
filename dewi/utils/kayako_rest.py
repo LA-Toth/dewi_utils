76a0ba6612ca95d3bf223e1b622b9eb30427d680
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2018-01-16 20:06:12 +0100

    utils/kayako_rest: add Connection cls and ensure_connection() method
    The previously existing Request class is good for one-shot HTTP requests,
    but usually an application uses multiple request, therefore it is better
    to use an object for every possible request.
    This is Connection.
    
    As a result the calculations can be done once, and reuse it multiple times,
    which makes the code a little bit faster.
    The object creation before calling fetch(), post() or put() is not
    necessary anymore, which makes the code cleaner.

--- dewi/utils/kayako_rest.py
+++ dewi/utils/kayako_rest.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Laszlo Attila Toth
+# Copyright 2017-2018 Laszlo Attila Toth
 # Distributed under the terms of the GNU Lesser General Public License v3
 
 
@@ -97,6 +97,83 @@ class Request:
             raise AlreadyProcessed()
 
         self._processed_once = True
+
+
+class Connection:
+    def __init__(self, host: Host):
+        self._host = host
+        self._salt = str(random.getrandbits(32))
+        self._signature = self._generate_signature()
+        self._params = dict()
+        self._params['apikey'] = self._host.api_key
+        self._params['salt'] = self._salt
+        self._params['signature'] = self._signature
+
+    @property
+    def host(self):
+        return self._host
+
+    def _generate_signature(self):
+        digest = hmac.new(
+            self._host.secret.encode('UTF-8'),
+            msg=self._salt.encode('UTF-8'), digestmod=hashlib.sha256).digest()
+        return base64.b64encode(digest)
+
+    def _generate_url(self, endpoint: str):
+        return f'{self._host.api_url}?e={endpoint}'
+
+    def _generate_fetch_url(self, endpoint: str, params: typing.Optional[dict] = None):
+        return f'{self._host.api_url}?e={endpoint}&' + self._prepare_parameters(params)
+
+    def _prepare_parameters(self, params: typing.Optional[dict]) -> str:
+        params_ = dict(params) if params else dict()
+        params_.update(self._params)
+
+        result = list()
+        for key, value in params_.items():
+            if isinstance(value, (list, tuple)):
+                for v in value:
+                    result.append((key + '[]', v))
+            else:
+                result.append((key, value))
+
+        return urllib.parse.urlencode(result)
+
+    def _urlopen(self, *args, **kwargs):
+        if self._host.unsafe_ssl:
+            ctx = ssl.create_default_context()
+            ctx.check_hostname = False
+            ctx.verify_mode = ssl.CERT_NONE
+
+            kwargs.update(dict(context=ctx))
+
+        return urllib.request.urlopen(*args, **kwargs)
+
+    def fetch(self, endpoint: str, params: typing.Optional[dict] = None):
+        url = self._generate_fetch_url(endpoint, params)
+        with self._urlopen(url) as response:
+            return response.read()
+
+    def put(self, endpoint: str, params: typing.Optional[dict]):
+        url = self._generate_url(endpoint)
+        request = urllib.request.Request(url, method='PUT',
+                                         data=self._prepare_parameters(params).encode('UTF-8'))
+        with self._urlopen(request) as response:
+            return response.read()
+
+    def post(self, endpoint: str, params: typing.Optional[dict]):
+        url = self._generate_url(endpoint)
+        request = urllib.request.Request(url, method='POST',
+                                         data=self._prepare_parameters(params).encode('UTF-8'))
+        with self._urlopen(request) as response:
+            return response.read()
+
+
+def ensure_connection(host_or_connection: typing.Union[Host, Connection]) -> Connection:
+    if isinstance(host_or_connection, Connection):
+        return host_or_connection
+    else:
+        return Connection(host_or_connection)
 
 
 def get_response_as_dict(h: Host, endpoint: str, params: typing.Optional[dict] = None):
