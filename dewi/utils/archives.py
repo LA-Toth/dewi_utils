7f398f3d2b8f47d75d7a0434e9d3ffa717b32f34
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2018-04-09 16:26:44 +0200

    dewi.utils.archives: contains an UnZip class to unzip / filter .zip files
    The UnZip._filter is for filtering the file content depeneding on
    the extract() method's parameter.
    extract_all() and extract_filtered() are wrapper methods.

---
+++ dewi/utils/archives.py
@@ -0,0 +1,53 @@
+import os
+import subprocess
+from zipfile import ZipFile
+
+from dewi.core.context_managers import in_directory
+
+
+class UnZip:
+    def __init__(self, zip_file, output_dir):
+        self._zip_file = zip_file
+        self._output_dir = output_dir
+
+    def extract(self, filtered: bool = False):
+        if not filtered and os.path.exists("C:\\Program Files\\7-Zip\\7z.exe"):
+            subprocess.run(
+                [
+                    "C:\\Program Files\\7-Zip\\7z.exe",
+                    "x",
+                    "-o" + self._output_dir,
+                    self._zip_file
+                ])
+        elif not filtered and os.path.exists("/usr/bin/7z"):
+            subprocess.run(
+                [
+                    "/usr/bin/7z",
+                    "x",
+                    "-o" + self._output_dir,
+                    self._zip_file
+                ])
+        else:
+            with ZipFile(self._zip_file) as f:
+                with in_directory(self._output_dir):
+                    members = self._get_member_list(f, filtered)
+                    f.extractall(members=members)
+
+    def extract_all(self):
+        self.extract()
+
+    def extract_filtered(self):
+        self.extract(True)
+
+    def _get_member_list(self, f: ZipFile, filtered: bool):
+        members = None
+        if filtered:
+            members = [m.filename for m in f.infolist() if self._filter(m.filename)]
+        return members
+
+    def _filter(self, path: str):
+        """
+        Example:
+        return path.startswith('info') or path == 'a.file.txt'
+        """
+        return True
