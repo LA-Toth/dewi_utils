db11ae201eb86e7701b6d1e8b26f2312c09bbc49
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2021-11-02 12:18:16 +0100

    Add files.python_repo_hash_md5 to have an a checksum
    __pycache__ and /.git dirs are ignored.
    If any file is changed (mtime, mode, size), the checksum differs.
    The content is not checked.

--- dewi_utils/files.py
+++ dewi_utils/files.py
@@ -1,7 +1,12 @@
-# Copyright 2019 Laszlo Attila Toth
+# Copyright 2019-2021 Laszlo Attila Toth
 # Distributed under the terms of the GNU Lesser General Public License v3
+
+import hashlib
+import os
 import os.path
 import typing
+
+from dewi_core.logger import log_info
 
 
 def find_file_recursively(filename: str, directory_name: typing.Optional[str] = None) -> typing.Optional[str]:
@@ -31,3 +36,50 @@ def find_file_recursively(filename: str, directory_name: typing.Optional[str] = 
         fname = maybe_file(filename, directory_name)
 
     return fname
+
+
+def _collect_entries(rootdir: str, basedir: str):
+    """
+    Collectes entries in rootdir's basedir directory which is always relateive to rootdir.
+    """
+
+    files = []
+    dirs = []
+
+    for entry in os.listdir(os.path.join(rootdir, basedir)):
+        rel_path = os.path.join(basedir, entry)
+        full_path = os.path.join(rootdir, rel_path)
+        isdir = os.path.isdir(full_path)
+        if isdir and (rel_path in ('./.git', './.pytest_cache') or entry == '__pycache__'):
+            continue
+
+        st = os.stat(full_path, follow_symlinks=False)
+
+        (dirs if isdir else files).append((rel_path, dict(isdir=isdir, path=rel_path, size=(0 if isdir else st.st_size),
+                                                          mode=st.st_mode, omode=f'{st.st_mode:04o}',
+                                                          mtime=int(st.st_mtime))))
+
+    for rel_path, entry in sorted(dirs):
+        yield entry
+        yield from _collect_entries(rootdir, rel_path)
+
+    for _, entry in sorted(files):
+        yield entry
+
+
+def python_repo_hash_md5(root_dir: str, *, verbose: bool = False):
+    """
+    Return MD5 hash's hexdigest bases on non-git non-pycache entries of the root_dir.
+
+    The purpose is to check if two directory is identical except the modification dates.
+    The two directories can be on different machines when the file transfer would be costly.
+    """
+    m = hashlib.md5()
+    for e in _collect_entries(root_dir, '.'):
+        if verbose:
+            log_info('Processing e', e)
+        m.update(
+            f"path={e['path']}\tisdir={e['isdir']}\tsize={e['size']}\tmode={e['mode']:03o}\tmtime={e['mtime']}\n"
+                .encode('UTF-8'))
+
+    return m.hexdigest()
